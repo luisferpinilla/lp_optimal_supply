3ba1caeda1cac0aef2a06f61ee4ab90ff4558cb4
Luis Pinilla <luisferpinilla@gmail.com>
2021-12-07 06:09:26 -0500

    Ajustar para usar la clase Event

--- sim_utils/event_queue.py
+++ sim_utils/event_queue.py
@@ -1,3 +1,8 @@
+from event import Event
+from sim_utils.event_type import EventType
+
+
+
 class EventQueue():
     def __init__(self, initial_clock=0) -> None:
         self.current_clock = initial_clock
@@ -5,13 +10,15 @@ class EventQueue():
 
     def add_event(self, time_clock:int, event_name:str):
         
+        event = Event(clock=time_clock,type_of_event=EventType.ARRIVAL, event_name=event_name)
+
         if time_clock <= self.current_clock:
             raise Exception('you cannot create an event in the past')
         else:
             if not time_clock in self.queue.keys():
                 self.queue[time_clock] = list()
         
-            self.queue[time_clock].append(event_name)
+            self.queue[time_clock].append(event)
 
     def pop_up_events(self)->tuple:
         event_list_times = self.queue.keys()
