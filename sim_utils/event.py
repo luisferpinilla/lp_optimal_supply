2fff634cfe66d2401ba6ae5eb0c24ef638603d85
Luis Pinilla <luisferpinilla@gmail.com>
2021-12-07 06:09:13 -0500

    Crear clase Event

--- sim_utils/event.py
+++ sim_utils/event.py
@@ -1,7 +1,6 @@
-
-
+from event_type import EventType
 class Event():
-    def __init__(self, clock:int, type_of_event:str, event_name:str) -> None:
+    def __init__(self, clock:int, type_of_event:EventType, event_name:str) -> None:
         self.clock = clock
         self.event_type = type_of_event
         self.event_name = event_name
