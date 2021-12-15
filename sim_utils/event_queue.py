da1b8f890eea82f90305cf0aff4c294f071d33ac
Luis Pinilla <luisferpinilla@gmail.com>
2021-12-15 17:30:58 -0500

    docstrings

--- sim_utils/event_queue.py
+++ sim_utils/event_queue.py
@@ -8,9 +8,18 @@ class EventQueue():
         self.current_clock = initial_clock
         self.queue = dict()
 
-    def add_event(self, time_clock:int, event_name:str):
-        
-        event = Event(clock=time_clock,type_of_event=EventType.ARRIVAL, event_name=event_name)
+    def add_event(self, time_clock:int, event_name:str, event_type:EventType):
+        """Add an event to event queue
+
+        Args:
+            time_clock (int): time when the event will ocuur
+            event_name (str): name of the event
+            event_type (EventType): event type: Arrival/delivery
+
+        Raises:
+            Exception: you can't create an event in the pass
+        """
+        event = Event(clock=time_clock,type_of_event=event_type, event_name=event_name)
 
         if time_clock <= self.current_clock:
             raise Exception('you cannot create an event in the past')
@@ -21,10 +30,15 @@ class EventQueue():
             self.queue[time_clock].append(event)
 
     def pop_up_events(self)->tuple:
+        """Bring the followin event
+
+        Returns:
+            tuple: [description]
+        """
         event_list_times = self.queue.keys()
         event_list_times = sorted(event_list_times, reverse=False)
         if len(event_list_times)>0:
             event = self.queue[event_list_times[0]]
-            self.queue.pop(event)
-
-        return 
+            return self.queue.pop(event)
+        else:
+            return None 
