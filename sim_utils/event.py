24772e79f26545b5d09626482edc0eae55953267
Luis Pinilla <luisferpinilla@gmail.com>
2021-12-15 17:34:46 -0500

    docstrings

--- sim_utils/event.py
+++ sim_utils/event.py
@@ -7,14 +7,29 @@ class Event():
     
     @property
     def get_clock(self)-> int:
+        """get the time when the event occurs
+
+        Returns:
+            int: time when the event occurs
+        """
         return self.clock
 
     @property
     def get_event_type(self)->EventType:
+        """Get the event type
+
+        Returns:
+            EventType: Type of the event
+        """
         return self.event_type
     
     @property
     def get_event_name(self)->str:
+        """get the name of the event
+
+        Returns:
+            str: event name
+        """
         return self.event_name
 
         
