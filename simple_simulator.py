ffe4e4999976b92e4f72a8605091c3e61bbea84d
Luis Pinilla <luisferpinilla@gmail.com>
2021-12-15 22:54:21 -0500

    add purchase order method

--- simple_simulator.py
+++ simple_simulator.py
@@ -2,6 +2,13 @@ from sim_utils.simple_simulator import SimpleSimulator
 
 
 def main():
+    # Create an instance of simulator
     simulador = SimpleSimulator(initial_inventory=500, lead_time=3)
 
+    # load forecast data file
+    # load initial inventory file
+    # load current purchase orders
+    # load leadtime file
+    # run simulator
     
+
