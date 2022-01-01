2cbcbc90f5025bc77e73cee05e999c451e8068f6
Luis Pinilla <luisferpinilla@gmail.com>
2022-01-01 08:56:23 -0500

    Ajuste de cola de eventos

--- sim_utils/event_queue.py
+++ sim_utils/event_queue.py
@@ -1,11 +1,10 @@
 from event import Event
-from sim_utils.event_type import EventType
+from event_type import EventType
 
 
 
 class EventQueue():
-    def __init__(self, initial_clock=0) -> None:
-        self.current_clock = initial_clock
+    def __init__(self) -> None:
         self.queue = dict()
 
     def add_event(self, time_clock:int, event_name:str, event_type:EventType):
@@ -15,30 +14,28 @@ class EventQueue():
             time_clock (int): time when the event will ocuur
             event_name (str): name of the event
             event_type (EventType): event type: Arrival/delivery
-
-        Raises:
-            Exception: you can't create an event in the pass
         """
         event = Event(clock=time_clock,type_of_event=event_type, event_name=event_name)
-
-        if time_clock <= self.current_clock:
-            raise Exception('you cannot create an event in the past')
-        else:
-            if not time_clock in self.queue.keys():
-                self.queue[time_clock] = list()
+        if not time_clock in self.queue.keys():
+            self.queue[time_clock] = list()
         
-            self.queue[time_clock].append(event)
+        self.queue[time_clock].append(event)
 
-    def pop_up_events(self)->tuple:
-        """Bring the followin event
+    def pop_up_events(self, time_clock:int)->list:
+        """returns an event list at the time_clock
+        Args:
+            time_clock (int): time_clock when the events occurs
 
         Returns:
-            tuple: [description]
+            list: list of events that occurs
         """
-        event_list_times = self.queue.keys()
-        event_list_times = sorted(event_list_times, reverse=False)
-        if len(event_list_times)>0:
-            event = self.queue[event_list_times[0]]
-            return self.queue.pop(event)
+        if time_clock in self.queue.keys():
+            eventos = self.queue[time_clock]
+            # sacar los eventos de la lista
+            return self.queue.pop(time_clock, list())
         else:
-            return None 
+            return list()
+
+    @property
+    def has_more_events(self)->int:
+        return len(self.queue)
