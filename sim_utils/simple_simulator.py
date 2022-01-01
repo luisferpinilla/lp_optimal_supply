ad92b14a1806ae9e278c96c15fda22265de956a3
Luis Pinilla <luisferpinilla@gmail.com>
2022-01-01 11:54:59 -0500

    Ajuste de simulador

--- sim_utils/simple_simulator.py
+++ sim_utils/simple_simulator.py
@@ -1,52 +1,89 @@
 import numpy as np
 from event_queue import EventQueue
 from event_type import EventType
+from event import Event
+
 
 class SimpleSimulator(object):
-    def __init__(self, initial_inventory=0.0, lead_time=1, capacity=np.inf)  -> None:
+    def __init__(self, initial_inventory=0.0, lead_time=1, reorder_point=10, quantity_to_order=10, safety_Stock=0.0) -> None:
         super().__init__()
         self.clock = 0
-        self.event_queue = EventQueue()       
-        self.initial_inventory=initial_inventory
+        self.event_queue = EventQueue()
+        self.inventory = initial_inventory
         self.lead_time = lead_time
+        self.quantity_ordered = 0
+        self.reorder_point = reorder_point
+        self.quantity_to_order = quantity_to_order
+        self.safety_stock = safety_Stock
 
+    @property
+    def inventory_position(self) -> int:
+        """return the current inventory position
 
-    
+        Returns:
+            int: curent inventory position
+        """
+        return self.inventory + self.quantity_ordered
+
     def advance_time(self):
         # Obtener la lista de eventos para el time en clock
         event_list = self.event_queue.pop_up_events(self.clock)
         # Para cada evento, ejecutar sus acciones
         for event in event_list:
-            if event:
-                print('\t',event.get_event_type, event.get_event_name)
+            if event.get_event_type == EventType.DEMAND:
+                self.handle_demand_event(event)
+            elif event.get_event_type == EventType.PURCHASE_ORDER:
+                pass
+            elif event.get_event_type == EventType.ARRIVAL:
+                self.handle_arrival_event(event=event)
+            else:
+                pass
 
         self.clock += 1
 
-    def add_purchase_order(self, time:int, quantity:float):
-        """add a purchase order to the event queue.
+        print(self.clock, ':', 'Inventario:', self.inventory, 'ordered:',
+              self.quantity_ordered, 'inventory_position', self.inventory_position)
 
-        Args:
-            time (int): time when the purchase order is created
-            quantity (float): cuantity ordered
-        """
-        self.event_queue.add_event(time, event_name='PO', event_type=EventType.PURCHASE_ORDER)
+    def add_demand_event(self, time_clock: int, quantity: int):
+        print(f'\tventa por {quantity}')
+        event = Event(clock=time_clock, type_of_event=EventType.DEMAND,
+                      event_name='Pedido', document={'qty': quantity})
+        self.event_queue.add_event(event=event)
 
-        pass
+    def add_purchase_order(self):
 
-    def handle_purchase_order(self):
-        pass
+        qty_to_order = self.safety_stock + self.quantity_to_order - self.inventory_position
+        print('\tordenando', qty_to_order)
+        event = Event(clock=self.clock + self.lead_time,
+                      type_of_event=EventType.ARRIVAL,
+                      event_name='Pedido',
+                      document={'qty': qty_to_order})
+        self.quantity_ordered += qty_to_order
+        self.event_queue.add_event(event=event)
 
-    def handle_demand_event(self):
-        pass
+    def handle_arrival_event(self, event: Event):
+        qty = event.get_document['qty']
+        print(f'\tLlegada por {qty}')
+        self.inventory += qty
+        self.quantity_ordered -= qty
+
+    def handle_demand_event(self, event: Event):
+        # Obtener inforamciòn del pedido
+        qty = event.get_document['qty']
+        print(
+            f'\tatendiendo venta por {qty} con inventario de {self.inventory}')
+        self.inventory -= qty
+        if self.inventory_position <= self.reorder_point:
+            self.add_purchase_order()
 
 
-sim = SimpleSimulator(initial_inventory=100, lead_time=5)
-sim.event_queue.add_event(time_clock=10, event_name='test llegada 10', event_type=EventType.PURCHASE_ORDER)
-sim.event_queue.add_event(time_clock=12, event_name='test llegada 12', event_type=EventType.ARRIVAL)
-sim.event_queue.add_event(time_clock=14, event_name='test llegada 14', event_type=EventType.ARRIVAL)
-sim.event_queue.add_event(time_clock=11, event_name='test llegada 11', event_type=EventType.ARRIVAL)
+sim = SimpleSimulator(initial_inventory=20, lead_time=3,
+                      reorder_point=12, quantity_to_order=25, safety_Stock=5)
 
-for t in range(20):
+for i in range(200):
+    sim.event_queue.add_event(Event(clock=i, type_of_event=EventType.DEMAND,
+                              event_name='Pedido', document={'qty': np.random.randint(low=1, high=20)}))
+
+
+for t in range(200):
     sim.advance_time()
-
-
