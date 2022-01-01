1d58022c6aa15072c0362169634f8fed910ee408
Luis Pinilla <luisferpinilla@gmail.com>
2022-01-01 09:11:42 -0500

    Mejora de definicion de constructor de even queue

--- sim_utils/event_queue.py
+++ sim_utils/event_queue.py
@@ -7,19 +7,16 @@ class EventQueue():
     def __init__(self) -> None:
         self.queue = dict()
 
-    def add_event(self, time_clock:int, event_name:str, event_type:EventType):
-        """Add an event to event queue
+    def add_event(self, event:Event):
+        """add an event to the queue
 
         Args:
-            time_clock (int): time when the event will ocuur
-            event_name (str): name of the event
-            event_type (EventType): event type: Arrival/delivery
+            event (Event): event to add
         """
-        event = Event(clock=time_clock,type_of_event=event_type, event_name=event_name)
-        if not time_clock in self.queue.keys():
-            self.queue[time_clock] = list()
+        if not event.get_clock in self.queue.keys():
+            self.queue[event.get_clock] = list()
         
-        self.queue[time_clock].append(event)
+        self.queue[event.get_clock].append(event)
 
     def pop_up_events(self, time_clock:int)->list:
         """returns an event list at the time_clock
