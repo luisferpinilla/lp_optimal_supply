823f58ea8174880730f277cbc2cdb0a297e88666
Luis Pinilla <luisferpinilla@gmail.com>
2022-01-01 11:54:43 -0500

    Ajuste de pruebas para seguir estandar python

--- sim_utils/test.py
+++ sim_utils/test.py
@@ -1,18 +1,16 @@
+import unittest
+from unittest.main import main
 from event_queue import EventQueue, Event, EventType
+import numpy as np
 
-print('inicializando queue')
-queue = EventQueue()
 
-print('agregando algunos eventos')
-queue.add_event(Event(clock=10, type_of_event=EventType.DEMAND, event_name='Test demanda 10'))
-queue.add_event(Event(clock=14, type_of_event=EventType.DEMAND, event_name='Test demanda 14'))
-queue.add_event(Event(clock=13, type_of_event=EventType.DEMAND, event_name='Test demanda 13'))
-queue.add_event(Event(clock=11, type_of_event=EventType.DEMAND, event_name='Test demanda 11'))
-queue.add_event(Event(clock=10, type_of_event=EventType.DEMAND, event_name='Test2 demanda 10'))
-print('pop up algunos eventos')
-for t in range(20):
-    print(f'time clock = {t}:')
-    events = queue.pop_up_events(t)
-    for event in events:
-        print('\t',event.get_event_type, event.get_event_name, 'tiene más?', queue.has_more_events) 
-    
+class pruebas(unittest.TestCase):
+
+    def test_queue(self):
+        self.assertTrue(True)
+
+    def test_value(self):
+        self.assertEqual(first=True, second=False, msg='Bad!!!')
+
+if __name__ == '__main__':
+    unittest.main()    
